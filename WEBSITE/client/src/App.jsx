177be8fa70b7a4ef005f7e0189f85a6c77475b15
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-23 11:22:00 +0530

    added some neccessary image to create a pwa

--- WEBSITE/client/src/App.jsx
+++ WEBSITE/client/src/App.jsx
@@ -6,7 +6,6 @@ import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import PrivateRoute from './components/PrivateRoute';
-import Disease from './pages/Disease';
 import Chat from './pages/Chat';
 
 export default function App() {
@@ -17,7 +16,6 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/disease' element={<Disease />} />
         <Route element={<PrivateRoute />} >
           <Route path='/profile' element={<Profile />} />
           <Route path='/chat' element={<Chat />} />
--- WEBSITE/client/vite.config.js
+++ WEBSITE/client/vite.config.js
@@ -1,5 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const manifestForPlugin = {
+  registerType:'autoUpdate',
+  includeAssests:['images/favicon.ico', "images/apple-touch-icon.png"],
+  manifest:{
+    name:"YieldSmart",
+    short_name:"YieldSmart",
+    description:"YieldSmart help to maximizing yield and minimizing waste for a sustainable agricultural future",
+    icons:[{
+      src: '/images/logo192.png',
+      sizes:'192x192',
+      type:'image/png',
+    },
+    {
+      src:'/images/logo256.png',
+      sizes:'256x256',
+      type:'image/png',
+    },
+    {
+      src: '/images/logo384.png',
+      sizes:'384x384',
+      type:'image/png',
+    },
+    {
+      src: '/images/logo512.png',
+      sizes:'512x512',
+      type:'image/png',
+    }
+  ],
+  theme_color:'#181818',
+  background_color:'#e0cc3b',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +49,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
