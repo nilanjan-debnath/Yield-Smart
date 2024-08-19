e39bdd2972e78f91227449435b88460a6010d06d
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-08-19 12:06:37 +0530

    delete unwanted api routes

--- WEBSITE/api/contollers/user.controller.js
+++ WEBSITE/api/contollers/user.controller.js
@@ -21,19 +21,3 @@ export const output = async (req, res, next) => {
         next(error);
     }
 };
-
-export const imgOutput = async (req, res, next) => {
-    try{
-        const response = await axios.post("https://yield-smart-agent.onrender.com/image/", {
-            image: req.body.image
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json'
-            }
-        });
-        res.status(200).json(response.data);
-    }catch(error){
-        return next(error);
-    }
-}
--- WEBSITE/api/routes/user.route.js
+++ WEBSITE/api/routes/user.route.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { test, output, imgOutput } from "../contollers/user.controller.js";
+import { test, output} from "../contollers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/output",verifyToken, output);
-router.post("/imageoutput", verifyToken, imgOutput);
 
 export default router;
